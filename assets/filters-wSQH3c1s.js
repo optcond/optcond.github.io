631d317f8627683548f1e0b6e00e6880866753fe
optcond <optimalcondition@tutanota.com>
2025-12-22 14:28:54 +0300

    personal wallet balance display

--- assets/filters-wSQH3c1s.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-CUb4UggS.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-C0dUZOLO.js";import"./index-8jBMxRYr.js";import"./query-CpGXJd7o.js";import"./recharts-CblCIUsG.js";import"./lucide-Chui0JB9.js";import"./fallback-CXhlFtAQ.js";const d=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M0 3a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1Zm2.63 5.25a1 1 0 0 1 1-1h8.75a1 1 0 1 1 0 2H3.63a1 1 0 0 1-1-1Zm2.62 5.25a1 1 0 0 1 1-1h3.5a1 1 0 0 1 0 2h-3.5a1 1 0 0 1-1-1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{d as filtersSvg};
---
+++ assets/refresh-BLbRO-Ij.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-9r3YHRk8.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-CTy3mUg_.js";import"./index-BN_TAcui.js";import"./query-CpGXJd7o.js";import"./recharts-CblCIUsG.js";import"./lucide-Chui0JB9.js";import"./fallback-RMwTEPra.js";const d=o`<svg fill="none" viewBox="0 0 14 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M3.94 1.04a1 1 0 0 1 .7 1.23l-.48 1.68a5.85 5.85 0 0 1 8.53 4.32 5.86 5.86 0 0 1-11.4 2.56 1 1 0 0 1 1.9-.57 3.86 3.86 0 1 0 1.83-4.5l1.87.53a1 1 0 0 1-.55 1.92l-4.1-1.15a1 1 0 0 1-.69-1.23l1.16-4.1a1 1 0 0 1 1.23-.7Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{d as refreshSvg};
