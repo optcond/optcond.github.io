63812526a23a9e4464d39014f293358d8687106e
optcond <optimalcondition@tutanota.com>
2025-12-22 23:12:31 +0300

    walletconnect disabled

--- assets/cursor-wS3Yj2bk.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-9r3YHRk8.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-CTy3mUg_.js";import"./index-BN_TAcui.js";import"./query-CpGXJd7o.js";import"./recharts-CblCIUsG.js";import"./lucide-Chui0JB9.js";import"./fallback-RMwTEPra.js";const c=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{c as cursorSvg};
