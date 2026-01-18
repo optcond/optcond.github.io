67dac4843364182194a8616f76a9b0c819927c25
optcond <optimalcondition@tutanota.com>
2026-01-18 16:27:49 +0300

    ui improvements

---
+++ assets/all-wallets-SZt4S43t.js
@@ -0,0 +1,6 @@
+import{b as o}from"./core-BeNYzDuF.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-DRxKQfup.js";import"./index-WCnWGAth.js";import"./query-CpGXJd7o.js";import"./recharts-CblCIUsG.js";import"./lucide-DUO7GzwE.js";import"./fallback-CPdbtZai.js";const c=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{c as allWalletsSvg};
