0535fab361a7b712f80aa6ac05a70076f01751e5
optcond <optimalcondition@tutanota.com>
2026-01-18 21:51:48 +0300

    liqs&redemptions improvement

--- assets/arrow-right-pwwP0tGU.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-CW6_8Sca.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-DIhOrWI1.js";import"./index-Br4yXWID.js";import"./query-CpGXJd7o.js";import"./recharts-CblCIUsG.js";import"./lucide-DpEOl3rZ.js";import"./fallback-C8jaedC9.js";const d=o`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M1 7.99a1 1 0 0 1 1-1h7.58L7.12 4.53A1 1 0 1 1 8.54 3.1l4.16 4.17a1 1 0 0 1 0 1.41l-4.16 4.17a1 1 0 1 1-1.42-1.41l2.46-2.46H2a1 1 0 0 1-1-1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{d as arrowRightSvg};
