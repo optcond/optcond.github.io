9ad55fba3b57bde51b965c6c8094a8abc8919cbd
optcond <optimalcondition@tutanota.com>
2026-01-27 22:02:23 +0300

    buttons renamed

--- assets/all-wallets-Di-4Gr0J.js
+++
@@ -1,6 +0,0 @@
-import{b as o}from"./core-CqCDDbnG.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-Cj1SkHRM.js";import"./index-ByzhFkxc.js";import"./query-CpGXJd7o.js";import"./recharts-CblCIUsG.js";import"./lucide-CcWIjwUx.js";import"./fallback-Cd6zR8tD.js";const c=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{c as allWalletsSvg};
---
+++ assets/cursor-DrDFFt_V.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-CfTsrT4Z.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-BXzkv0nQ.js";import"./index-cuwd4wWD.js";import"./query-CpGXJd7o.js";import"./recharts-CblCIUsG.js";import"./lucide-CcWIjwUx.js";import"./fallback-CGi0BiVt.js";const c=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{c as cursorSvg};
