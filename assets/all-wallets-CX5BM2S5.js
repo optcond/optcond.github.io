33ebea8b00179699e9f6ca50da8967ad3adcdc19
optcond <optimalcondition@tutanota.com>
2026-04-15 20:41:11 +0300

    fixes

--- assets/all-wallets-CX5BM2S5.js
+++
@@ -1,6 +0,0 @@
-import{b as o}from"./core-DHCIDPq_.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-hD94N6VB.js";import"./index-BzS7Uv01.js";import"./query-CpGXJd7o.js";import"./recharts-CblCIUsG.js";import"./lucide-DcNH1Drq.js";import"./fallback-CYO4_1J8.js";const c=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{c as allWalletsSvg};
