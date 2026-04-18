b9e2bdb540cc7fa1962f389784e4cdb97a580467
optcond <optimalcondition@tutanota.com>
2026-04-18 11:56:31 +0300

    nft fix

---
+++ assets/external-link-DSVTjVGA.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-Bms9RALG.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-C4KuZpC6.js";import"./index-BBWPUJdv.js";import"./query-BPxH1CfV.js";import"./recharts-LuzJWhtN.js";import"./lucide-D1I07J1j.js";import"./fallback-C9McV6D5.js";const d=o`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M3.74 3.99a1 1 0 0 1 1-1H11a1 1 0 0 1 1 1v6.26a1 1 0 0 1-2 0V6.4l-6.3 6.3a1 1 0 0 1-1.4-1.42l6.29-6.3H4.74a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{d as externalLinkSvg};
