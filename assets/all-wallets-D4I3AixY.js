2a2db867c8a628d8079370af09818ea53cd3c003
optcond <optimalcondition@tutanota.com>
2026-04-19 00:51:12 +0300

    header improvement

--- assets/all-wallets-D4I3AixY.js
+++
@@ -1,6 +0,0 @@
-import{b as o}from"./core-BRvmkRS7.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-DZgxNwqs.js";import"./index-_3X0tk1L.js";import"./query-BPxH1CfV.js";import"./recharts-LuzJWhtN.js";import"./lucide-D1I07J1j.js";import"./fallback-HzVBTe4f.js";const c=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{c as allWalletsSvg};
