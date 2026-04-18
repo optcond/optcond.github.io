ca1341e9a7299f21d985ef32fcd3764a3080bfa0
optcond <optimalcondition@tutanota.com>
2026-04-18 12:23:53 +0300

    nft fix

---
+++ assets/chevron-top-CP223p2Q.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-BRvmkRS7.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-DZgxNwqs.js";import"./index-_3X0tk1L.js";import"./query-BPxH1CfV.js";import"./recharts-LuzJWhtN.js";import"./lucide-D1I07J1j.js";import"./fallback-HzVBTe4f.js";const d=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{d as chevronTopSvg};
--- assets/external-link-BW9btiuS.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-CK007vat.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-BsVAIVTv.js";import"./index-CT-gTV8q.js";import"./query-BPxH1CfV.js";import"./recharts-LuzJWhtN.js";import"./lucide-D1I07J1j.js";import"./fallback-Bn6Yisyy.js";const d=o`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M3.74 3.99a1 1 0 0 1 1-1H11a1 1 0 0 1 1 1v6.26a1 1 0 0 1-2 0V6.4l-6.3 6.3a1 1 0 0 1-1.4-1.42l6.29-6.3H4.74a1 1 0 0 1-1-1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{d as externalLinkSvg};
