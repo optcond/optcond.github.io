66097e5a398fb52ebc21f0ee8790918eb080385b
optcond <optimalcondition@tutanota.com>
2026-04-18 11:51:58 +0300

    nft fix

--- assets/arrow-top-F45oemLj.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-BJiJAkec.js";import"./supabase-CskpBK-R.js";import"./react-uYZGv8Ap.js";import"./events-DK53yLfj.js";import"./index.es-CdqnSbjp.js";import"./index-kJ_WDf8R.js";import"./query-BPxH1CfV.js";import"./recharts-LuzJWhtN.js";import"./lucide-D1I07J1j.js";import"./fallback-DUtGUdGZ.js";const d=o`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M7 13.99a1 1 0 0 1-1-1V5.4L3.54 7.86a1 1 0 0 1-1.42-1.41L6.3 2.28a1 1 0 0 1 1.41 0l4.17 4.17a1 1 0 1 1-1.41 1.41L8 5.4v7.59a1 1 0 0 1-1 1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{d as arrowTopSvg};
